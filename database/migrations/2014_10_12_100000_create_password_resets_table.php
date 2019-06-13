2523443b932b46624529a78bd841a07452f53839
KhaledBadawyM <khaledBadawi881@gmail.com>
2019-06-13 09:56:50 +0200

    First commit

---
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordResetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::table('users', function (Blueprint $table) {
+           $table->boolean('is_verified')->default(0);
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('password_resets');
+        Schema::table('users', function (Blueprint $table) {
+          $table->dropColumn('is_verified');
+        });
+    }
+}
